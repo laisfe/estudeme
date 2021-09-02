b78c297dfd01a55e307be3500e14eed275966f3a
Lais <laisffn@hotmail.com>
2021-09-02 14:26:53 -0300

    verifica se usuário já está logado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import firebase from 'firebase/app';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'estudeme';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    firebase.initializeApp(environment.firebase);
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log('user', user);
+      if (user) {
+        // User is signed in
+        this.router.navigate(['/students']);
+      } else {
+        // User is signed out
+        this.router.navigate(['/']);
+      }
+    });
+  }
 }
